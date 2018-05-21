f056c160c2f7bfb2d59dc23d04f8294f751b556d
Nik Molnar <nik.molnar@consbio.org>
2018-05-20 17:02:44 -0700

    Add basic Django config

---
+++ csrt_project/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^csrt/', include('seedsource_core.django.seedsource.urls')),
+    url(r'^accounts/', include('seedsource_core.django.accounts.urls')),
+    url(r'^', include('ncdjango.urls'))
+]
+
+# For local development, use staticfiles to serve downloads
+if settings.DEBUG:
+    from django.views.static import serve
+
+    urlpatterns += [
+        url(r'^downloads/(?P<path>.*)$', serve, {'document_root': settings.DATASET_DOWNLOAD_DIR})
+    ]
