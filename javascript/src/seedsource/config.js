835d795a139bc258e997928a51471cdf97a8e386
Bill Klinkow <klinkow@gmail.com>
2018-07-11 13:25:38 -0700

    update colors

--- javascript/src/seedsource/config.js
+++ javascript/src/seedsource/config.js
@@ -8,92 +8,92 @@ const layerLabels = {
         {
           serviceName: "wa_new_zones-pipo",
           label: "Washington (2002) ponderosa pine",
-          style: { color: "#FF00FF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "lio-2014-10-09-seed_zone",
           label: "Ontario",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "nesma-breedzone_pine",
           label: "Ontario Jack pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "oregon_seed_zones-ponderosa_pine",
           label: "Oregon ponderosa pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "oregon_seed_zones-western_white_pine",
           label: "Oregon western white pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "r9seedzones-r9seedzones",
           label: "Region 9",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "historic_seed_zones-historic_seed_zones",
           label: "Oregon 1966/1973",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "nesma-breedzone_spruce",
           label: "Ontario Black spruce",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "updatedcageneric-updatedcageneric",
           label: "California",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "wa_new_zones-pimo",
           label: "Washington (2002) western white pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "wa_new_zones-psme",
           label: "Washington (2002) Douglas-fir",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "wa_new_zones-thpl",
           label: "Washington (2002) western redcedar",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "atlantic_canada_seed_zones-atlantic_canada_seed_zones_wgs84",
           label: "Atlantic Canada",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "oregon_seed_zones-douglas_fir",
           label: "Oregon (1996) Douglas-fir",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "wa_new_zones-pico",
           label: "Washington (2002) lodgepole pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "oregon_seed_zones-lodgepole_pine",
           label: "Oregon (1996) lodgepole pine",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "oregon_seed_zones-western_red_cedar",
           label: "Oregon (1996) western redcedar",
-          style: { color: "#FFOOFF" }
+          style: { color: "#00FFFF" }
         },
         {
           serviceName: "us_eco_l3",
           label: "Level III Ecoregions",
-          style: { color: "#00FFFF" }
+          style: { color: "#7FFF00" }
         }
     ]
 }
