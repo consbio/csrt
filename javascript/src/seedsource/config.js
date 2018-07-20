8e9e7d200b6f2aabf1781f5e5c0ebdd9481f86ac
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-07-20 16:52:10 -0700

    Add species

--- javascript/src/seedsource/config.js
+++ javascript/src/seedsource/config.js
@@ -102,6 +102,34 @@ const config = {
         {
             name: 'artr',
             label: 'Great Basin sagebrush'
+        },
+        {
+            name: 'psme',
+            label: 'Douglas-fir',
+        },
+        {
+            name: 'pico',
+            label: 'Lodgepole pine',
+        },
+        {
+            name: 'piba',
+            label: 'Jack pine'
+        },
+        {
+            name: 'pipo',
+            label: 'Ponderosa pine',
+        },
+        {
+            name: 'pima',
+            label: 'Black spruce'
+        },
+        {
+            name: 'thpl',
+            label: 'Western red cedar',
+        },
+        {
+            name: 'pimo',
+            label: 'Western white pine',
         }
     ],
 
