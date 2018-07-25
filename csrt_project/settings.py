3d8fc2acde33b3d19e2e75eb7f3e0cea2731c034
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-07-24 17:08:03 -0700

    Add `STATIC_ROOT` to settings

--- csrt_project/settings.py
+++ csrt_project/settings.py
@@ -179,6 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = CONFIG.get('static-root', '/var/www/static/')
 
 CELERY_TRACK_STARTED = True
 CELERY_RESULT_BACKEND = 'django-db'
