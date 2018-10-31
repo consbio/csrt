69a3179cb67d5900efa14d61a1bd00468c25daaf
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-10-31 14:07:21 -0700

    Update Basin Big Sagrbrush name

--- javascript/src/seedsource/config.js
+++ javascript/src/seedsource/config.js
@@ -25,96 +25,106 @@ const config = {
     labels: [
         // Seedzones
         {
-          serviceName: "wa_new_zones-pipo",
-          label: "Washington (2002) ponderosa pine",
-          style: { color: "#00FFFF" }
+            serviceName: "wa_new_zones-pipo",
+            label: "Washington (2002) ponderosa pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "lio-2014-10-09-seed_zone",
-          label: "Ontario",
-          style: { color: "#00FFFF" }
+            serviceName: "lio-2014-10-09-seed_zone",
+            label: "Ontario",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "nesma-breedzone_pine",
-          label: "Ontario Jack pine",
-          style: { color: "#00FFFF" }
+            serviceName: "nesma-breedzone_pine",
+            label: "Ontario Jack pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "oregon_seed_zones-ponderosa_pine",
-          label: "Oregon ponderosa pine",
-          style: { color: "#00FFFF" }
+            serviceName: "oregon_seed_zones-ponderosa_pine",
+            label: "Oregon ponderosa pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "oregon_seed_zones-western_white_pine",
-          label: "Oregon western white pine",
-          style: { color: "#00FFFF" }
+            serviceName: "oregon_seed_zones-western_white_pine",
+            label: "Oregon western white pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "r9seedzones-r9seedzones",
-          label: "Region 9",
-          style: { color: "#00FFFF" }
+            serviceName: "r9seedzones-r9seedzones",
+            label: "Region 9",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "historic_seed_zones-historic_seed_zones",
-          label: "Oregon 1966/1973",
-          style: { color: "#00FFFF" }
+            serviceName: "historic_seed_zones-historic_seed_zones",
+            label: "Oregon 1966/1973",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "nesma-breedzone_spruce",
-          label: "Ontario Black spruce",
-          style: { color: "#00FFFF" }
+            serviceName: "nesma-breedzone_spruce",
+            label: "Ontario Black spruce",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "updatedcageneric-updatedcageneric",
-          label: "California",
-          style: { color: "#00FFFF" }
+            serviceName: "updatedcageneric-updatedcageneric",
+            label: "California",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "wa_new_zones-pimo",
-          label: "Washington (2002) western white pine",
-          style: { color: "#00FFFF" }
+            serviceName: "wa_new_zones-pimo",
+            label: "Washington (2002) western white pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "wa_new_zones-psme",
-          label: "Washington (2002) Douglas-fir",
-          style: { color: "#00FFFF" }
+            serviceName: "wa_new_zones-psme",
+            label: "Washington (2002) Douglas-fir",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "wa_new_zones-thpl",
-          label: "Washington (2002) western redcedar",
-          style: { color: "#00FFFF" }
+            serviceName: "wa_new_zones-thpl",
+            label: "Washington (2002) western redcedar",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "atlantic_canada_seed_zones-atlantic_canada_seed_zones_wgs84",
-          label: "Atlantic Canada",
-          style: { color: "#00FFFF" }
+            serviceName: "atlantic_canada_seed_zones-atlantic_canada_seed_zones_wgs84",
+            label: "Atlantic Canada",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "oregon_seed_zones-douglas_fir",
-          label: "Oregon (1996) Douglas-fir",
-          style: { color: "#00FFFF" }
+            serviceName: "oregon_seed_zones-douglas_fir",
+            label: "Oregon (1996) Douglas-fir",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "wa_new_zones-pico",
-          label: "Washington (2002) lodgepole pine",
-          style: { color: "#00FFFF" }
+            serviceName: "wa_new_zones-pico",
+            label: "Washington (2002) lodgepole pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "oregon_seed_zones-lodgepole_pine",
-          label: "Oregon (1996) lodgepole pine",
-          style: { color: "#00FFFF" }
+            serviceName: "oregon_seed_zones-lodgepole_pine",
+            label: "Oregon (1996) lodgepole pine",
+            style: { color: "#00FFFF" }
         },
         {
-          serviceName: "oregon_seed_zones-western_red_cedar",
-          label: "Oregon (1996) western redcedar",
-          style: { color: "#00FFFF" }
+            serviceName: "oregon_seed_zones-western_red_cedar",
+            label: "Oregon (1996) western redcedar",
+            style: { color: "#00FFFF" }
+        },
+        {
+            serviceName: '2013_revised_zones-2017_new_labels_liii_ecoregions_14june17',
+            label: 'CONUS Provisional',
+            style: { color: '#00FFFF' }
+        },
+        {
+            serviceName: 'shp_bdry_seed_source_control_zones-mndnr_dissolved',
+            label: 'Minnesota Seed Source Control',
+            style: { color: '#00FFFF' }
         },
 
         // Layers
         {
-          serviceName: "us_eco_l3",
-          label: "Level III Ecoregions",
-          style: { weight: 1, color: "#666", opacity: .5 }
+            serviceName: "us_eco_l3",
+            label: "Level III Ecoregions",
+            style: { weight: 1, color: "#666", opacity: .5 }
         },
         {
             serviceName: "cb_2017_us_county_20m",
@@ -126,7 +136,7 @@ const config = {
     species: [
         {
             name: 'artr',
-            label: 'Great Basin sagebrush'
+            label: 'Wyoming/Basin Big Sagebrush'
         },
         {
             name: 'psme',
@@ -179,12 +189,12 @@ const config = {
 
     constraints: {
         objects: Object.assign(coreConfig.constraints.objects, {
-            arwy: {
+            artr: {
                 component: SpeciesConstraint,
                 constraint: 'raster',
                 values: {
-                    species: 'arwy',
-                    label: 'Wyoming big sagebrush'
+                    species: 'artr',
+                    label: 'Wyoming/Basin Big Sagebrush'
                 },
                 serialize: serializeSpeciesConstraint
             }
@@ -197,8 +207,8 @@ const config = {
                 type: 'category',
                 items: [
                     {
-                        name: 'arwy',
-                        label: 'Wyoming big sagebrush',
+                        name: 'artr',
+                        label: 'Wyoming/Basin Big Sagebrush',
                         type: 'constraint'
                     }
                 ]
