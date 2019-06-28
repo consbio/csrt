b14c72cc0da97111fc10a2667a80ce9d9d1f2f65
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2019-06-27 18:36:25 -0700

    Select 'function' transfer method by default

---
+++ javascript/src/seedsource/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CoreApp from '../../../../seedsource-core/javascript/src/seedsource/components/App'
+import { store } from '../../../../seedsource-core/javascript/src/index'
+import { selectMethod } from '../../../../seedsource-core/javascript/src/actions/variables'
+
+export default class App extends React.Component {
+    componentDidMount() {
+        store.dispatch(selectMethod('function'))
+    }
+
+    render() {
+        return <CoreApp />
+    }
+}
