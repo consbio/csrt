4e530cb45f104bb71a8d9042cd2d79d1c9b0648d
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2019-07-15 12:00:31 -0700

    Fix "About" text

--- javascript/src/seedsource/components/About.jsx
+++ javascript/src/seedsource/components/About.jsx
@@ -27,21 +27,21 @@ export default () => {
             <p>&nbsp;</p>
             <h5 className="title is-5">Plants with genetic information</h5>
             <p className="about-text">
-                Like the Seedlot Selection Tool (SST), the CSRT uses user-determined climate variables and thresholds to
-                define seed transfer limits.
+                The CSRT uses genecological functions to map seed transfer limits of select species. The number of
+                species with genetic information will evolve in time as more genetic data becomes available.
             </p>
             <p>&nbsp;</p>
             <h5 className="title is-5">Plants without genetic information</h5>
             <p className="about-text">
-                The CSRT is capable of constraining seed transfer based on species distribution model (when available),
-                ecoregion, geographic distance and elevation. Please review the instructions, which have species-specific
-                recommendations for mapping seed transfer.
+                Like the Seedlot Selection Tool (SST), the CSRT uses user-determined climate variables and thresholds to
+                define seed transfer limits.
             </p>
             <p>&nbsp;</p>
             <h5 className="title is-5">Constraints</h5>
             <p className="about-text">
-                The CSRT is capable of constraining seed transfer based on: species distribution models (for some species)
-                and ecoregions.
+                The CSRT is capable of constraining seed transfer based on species distribution model (when available),
+                ecoregion, geographic distance and elevation. Please review the instructions, which have
+                species-specific recommendations for mapping seed transfer.
             </p>
         </div>
     )
